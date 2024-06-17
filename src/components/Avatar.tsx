c949c6de7b1b0057b278f283461e44e9620508c6
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-06-17 09:08:30 +0100

    started user implementation

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,16 @@
+interface AvatarProps {
+    userId: string,
+    isLarge?: boolean,
+    hasBorder?: boolean
+}
+    const Avatar: React.FC<AvatarProps> = (
+        { userId, isLarge, hasBorder}
+    ) => {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+    export default Avatar;
