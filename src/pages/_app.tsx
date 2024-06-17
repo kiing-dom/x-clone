ec397344b1185c512787a78b707f8bf91e1f59a4
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-06-17 05:34:38 +0100

    new components... > completed Button.tsx > started implentation of Modal.tsx component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import Layout from "@/components/Layout";
+import Modal from "@/components/Modal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+    <Modal isOpen />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 
 }
