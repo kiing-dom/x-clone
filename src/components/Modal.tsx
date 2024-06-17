8dd265d3e5518ade642c8cb0156eeae45ed4faab
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-06-17 05:46:50 +0100

    Modal.tsx component implemented

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,6 @@
 import { useCallback } from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import Button from "./Button";
 
 interface ModalProps  {
     isOpen?: boolean;
@@ -70,7 +72,69 @@ const Modal: React.FC<ModalProps> = ({
                         h-full
                         lg:h-auto
                 ">
-
+                    {/* Modal Content */}
+                    <div className="
+                        h-full
+                        lg:h-auto
+                        border-0
+                        rounded-lg
+                        shadow-lg
+                        relative
+                        flex
+                        flex-col
+                        w-full
+                        bg-black
+                        outline-none
+                        focus:outline-none
+                    ">
+                        {/* Modal Header */}
+                        <div className="
+                            flex
+                            items-center
+                            justify-between
+                            p-10
+                            rounded-t
+                        ">
+                            <h3 className="text-3xl font-semibold text-white">{title}</h3>
+                            <button
+                                className="
+                                    p-1
+                                    ml-auto
+                                    border-0
+                                    text-white
+                                    hover:opacity-70
+                                    transition
+                                "
+                                onClick={handleClose}
+                            >
+                                <AiOutlineClose size={20} />
+                            </button>
+                        </div>
+                        {/* Modal Body */}
+                        <div
+                            className="
+                                relative
+                                p-10
+                                flex-auto
+                            "
+                        >
+                            {body}
+                        </div>
+                        {/* Modal Footer*/}
+                        <div className="
+                            flex flex-col gap-2 p-10
+                        ">
+                            <Button 
+                                disabled={disabled} 
+                                label={actionLabel} 
+                                secondary 
+                                fullWidth
+                                large 
+                                onClick={handleSubmit}
+                            />
+                            {footer}
+                        </div>
+                    </div>
                 </div>
 
             </div>
