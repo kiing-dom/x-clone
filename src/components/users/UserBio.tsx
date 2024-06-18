4a65fe4f015a0ad396ff31fbc82b75ee4b799c2a
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-06-18 10:02:59 +0100

    user profile complete

--- src/components/users/UserBio.tsx
+++ src/components/users/UserBio.tsx
@@ -4,6 +4,7 @@ import useUser from "@/hooks/useUser";
 import { useSession } from "next-auth/react";
 import { useMemo } from 'react';
 import Button from '../Button';
+import { BiCalendar } from 'react-icons/bi';
 
 interface UserBioProps {
     userId: string;
@@ -37,6 +38,31 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
                         secondary
                     />
                 )}
+            </div>
+            <div className='mt-8 px-4'>
+                <div className='flex flex-col'>
+                    <p className='text-white text-2xl font-semibold'>{fetchedUser?.name}</p>
+                    <p className='text-neutral-500 text-md'>@{fetchedUser?.username}</p>
+                </div>
+                <div className='flex flex-col mt-4'>
+                    <p className='text-white'>{fetchedUser.bio}</p>
+                    <div className='flex flex-row items-center gap-2 mt-4 text-neutral-500'>
+                        <BiCalendar size={24} />
+                        <p className=''>
+                            Joined {createdAt}
+                        </p>
+                    </div>
+                </div>
+                <div className='flex flex-row items-center mt-4 gap-6'>
+                    <div className='flex flex-row items-center gap-1'>
+                        <p className='text-white'>{fetchedUser?.followingIds.length}</p>
+                        <p className='text-neutral-500'>Following</p>
+                    </div>
+                    <div className='flex flex-row items-center gap-1'>
+                        <p className='text-white'>{fetchedUser?.followersCount || 0}</p>
+                        <p className='text-neutral-500'>Followers</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
