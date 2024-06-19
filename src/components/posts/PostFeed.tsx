23cbe9e9b08aba05e4eccd4e7b9e0f77f510e711
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-06-19 08:53:34 +0100

    posts can be displayed

--- src/components/posts/PostFeed.tsx
+++ src/components/posts/PostFeed.tsx
@@ -1,12 +1,22 @@
-interface PostFeedProps {
+import usePosts from "@/hooks/usePosts";
+import PostItem from "./PostItem";
 
+interface PostFeedProps {
+    userId?: string
 }
 
-const PostFeed = () => {
+const PostFeed: React.FC<PostFeedProps> = ({ userId }) => {
+    const { data: posts = [] } = usePosts();
     return (
-        <div>
-
-        </div>
+        <>
+        {posts.map((post: Record<string, any>) => (
+            <PostItem 
+                userId={userId}
+                key={post.id}
+                data={post}
+            />
+        ))}
+        </>
     )
 }
 
