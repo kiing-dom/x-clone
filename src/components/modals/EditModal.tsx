9db5200e0bc5f5100760e9c91271d05d54bc539a
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-06-19 07:10:45 +0100

    uploading images works now

--- src/components/modals/EditModal.tsx
+++ src/components/modals/EditModal.tsx
@@ -7,6 +7,7 @@ import { useCallback, useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
 import Modal from "../Modal";
 import Input from "../Input";
+import ImageUpload from "../ImageUpload";
 
 interface EditModalProps {
 
@@ -66,6 +67,18 @@ const EditModal = () => {
 
     const bodyContent = (
         <div className="flex flex-col gap-4">
+            <ImageUpload
+                value={profileImage}
+                disabled={isLoading}
+                onChange={(image) => setProfileImage(image)}
+                label='Upload Profile Image'
+            />
+            <ImageUpload
+                value={coverImage}
+                disabled={isLoading}
+                onChange={(image) => setCoverImage(image)}
+                label='Upload Cover Image'
+            />
             <Input
                 placeholder="Name"
                 onChange={(e) => setName(e.target.value)}
