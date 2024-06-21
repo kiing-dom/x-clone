66d65e5da15974469e6d3dcebdcdc71675a1c698
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-06-21 05:35:54 +0100

    added urls to separate comments from replies

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -32,7 +32,9 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
         try {
             setIsLoading(true);
 
-            await axios.post('/api/posts', { body });
+            const url = isComment ? `/api/comments?postId${postId}` : '/api/posts';
+
+            await axios.post(url, { body });
 
             toast.success('Post Created!')
 
@@ -45,7 +47,7 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
         } finally {
             setIsLoading(false)
         }
-    }, [body, mutatePosts])
+    }, [body, mutatePosts, isComment, postId])
 
     return (
         <div className="border-b-[1px] border-neutral-800 px-5 py-2">
