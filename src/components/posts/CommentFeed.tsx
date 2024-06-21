b361041e54f82d0418dcdde5b307ad3e7cb88f06
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-06-21 05:47:23 +0100

    created skeleton of CommentFeed.tsx | TODO: finish it + create CommentItem component

---
+++ src/components/posts/CommentFeed.tsx
@@ -0,0 +1,15 @@
+interface CommentFeedProps {
+    comments?: Record<string, any>[];
+}
+
+const CommentFeed: React.FC<CommentFeedProps> = ({ comments = [] }) => {
+    return ( 
+        <>
+            {comments.map((comment) => (
+                <div key={comment.id}></div>
+            ))}
+        </>
+     );
+}
+ 
+export default CommentFeed;
