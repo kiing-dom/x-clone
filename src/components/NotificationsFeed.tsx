f4b1c66fc9f4397baa43222958407eb79d47678e
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-06-22 07:25:31 +0100

    created NotificationsFeed component

--- src/components/NotificationsFeed.tsx
+++ src/components/NotificationsFeed.tsx
@@ -1,6 +1,58 @@
+import { BsTwitterX } from "react-icons/bs";
+
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useNotifications from "@/hooks/useNotifications";
+
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+
 const NotificationsFeed = () => {
+
+    const { data: session } = useSession();
+    const { data: currentUser, mutate: mutateCurrentUser } = useCurrentUser(session);
+    const { data: fetchedNotifications = [] } = useNotifications(currentUser?.currentUser.id);
+
+    useEffect(() => {
+        mutateCurrentUser();
+    }, [mutateCurrentUser]);
+
+    if(fetchedNotifications.length === 0) {
+        return (
+            <div 
+                className="
+                    text-neutral-500
+                    text-center
+                    p-6
+                    text-xl
+                "
+            >
+                No Notifications
+            </div>
+        )
+    }
+
     return ( 
-        <div></div>
+        <div className="flex flex-col">
+            {fetchedNotifications.map((notification: Record<string, any>) => (
+                <div
+                    key={notification.id}
+                    className="
+                        flex
+                        flex-row
+                        items-center
+                        p-6
+                        gap-4
+                        border-b-[1px]
+                        border-neutral-800
+                    "
+                >
+                    <BsTwitterX color="white" size={32} />
+                    <p className="text-white">
+                        {notification.body}
+                    </p>
+                </div>
+            ))}
+        </div>
      );
 }
  
